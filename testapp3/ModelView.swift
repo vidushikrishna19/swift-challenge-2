c241e5cd317909364e87884381ed906069607c21
Vidushi Krishna <vidushikris@gmail.com>
2025-08-22 22:06:53 +0800

    i added comments

--- testapp3/ModelView.swift
+++ testapp3/ModelView.swift
@@ -13,21 +13,22 @@ struct ModelView : View {
     var body: some View {
         RealityView { content in
             
-            let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
+            let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2))) // this creates the anchor for the model to be anchored onto
             
             if let model = try? await Entity.init(named: "tired_young_person") {
-                model.scale = [0.2, 0.2, 0.2]    // scale down if too big
-                model.position = [0.2, 0, 0]     // place it slightly beside the cube
+                model.scale = [0.2, 0.2, 0.2]
+                // scale down if too big
+                model.position = [0.2, 0, 0]
+                // place it slightly beside
                 anchor.addChild(model)
             } else {
                 print("could not load model")
                 
-                // Add the horizontal plane anchor to the scene
-                content.add(anchor)
-                content.camera = .spatialTracking
+                content.add(anchor) // NEED to add the anchor to the scene otherwise bro will not be anchored
+                content.camera = .spatialTracking // this is so that the camera can track the thingy
             }
         }
-                .ignoresSafeArea(.all)
+                .ignoresSafeArea(.all) // otherwise ts aint gonna work
         
     }
 }
