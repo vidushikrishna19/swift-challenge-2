a06f0f85a7fc68957e6fa968c050e761869fde5e
Vidushi Krishna <vidushikris@gmail.com>
2025-08-23 14:22:01 +0800

    made the ghost chase

--- testapp3/ModelView.swift
+++ testapp3/ModelView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import RealityKit
 
 struct ModelView : View {
+    @State private var model: Entity? = nil // i need to make reference to the model in this view
     
     var body: some View {
         RealityView { content in
@@ -21,6 +22,7 @@ struct ModelView : View {
                 model.position = [0.2, 0, 0]
                 // place it slightly beside
                 anchor.addChild(model)
+                
             } else {
                 print("could not load model")
                 
@@ -28,8 +30,29 @@ struct ModelView : View {
                 content.camera = .spatialTracking // this is so that the camera can track the thingy
             }
         }
-                .ignoresSafeArea(.all) // otherwise ts aint gonna work
-        
+        .ignoresSafeArea(.all) // otherwise ts aint gonna work
+        .task {
+            
+            while true { // if the model exists
+                if let model = model {
+                    if let cameraTransform = await RealityView.cameraTransform { // ts gives where the user's position acc is in the space
+                        let cameraPosition = cameraTransform.translation // where the person is
+                        let modelPosition = model.position(relativeTo: nil) // where the camera is
+                        
+                        var direction = cameraPosition - modelPosition
+                        let distance = length(direction)
+                        
+                        if distance > 0.3 {
+                            direction = normalize(direction)
+                            let speed: Float = 0.01
+                            model.position += direction * speed
+                            model.look(at: cameraPosition, from: model.position, relativeTo: nil)
+                        }
+                    }
+                }
+            }
+        }
+        try? await Task.sleep(nanoseconds: 16_000_000)
     }
 }
 
