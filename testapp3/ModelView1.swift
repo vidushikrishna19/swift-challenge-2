2b44149eb2cca157c7c4d4683832e7ac3d795b0a
Vidushi Krishna <vidushikris@gmail.com>
2025-08-30 14:12:20 +0800

    changed the stuff

---
+++ testapp3/ModelView1.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  idoewjkd43e
+//
+//  Created by Vidushi Krishna on 23/8/25.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ModelView1 : View {
+
+    var body: some View {
+        RealityView { content in
+                    let model = try! await ModelEntity.init(named: "tired_young_person")
+                    model.scale = [0.01, 0.01, 0.01]
+                    model.position = [0, 0.05, 0]
+                    
+                    // Create horizontal plane anchor for the content
+                    let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
+                    anchor.addChild(model)
+                    // Add the horizontal plane anchor to the scene
+                    content.add(anchor)
+                    
+                    content.camera = .spatialTracking
+                }
+        .edgesIgnoringSafeArea(.all)
+            }
+        }
+
+
+#Preview {
+    ModelView1()
+}
+
