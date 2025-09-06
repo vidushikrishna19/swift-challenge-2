94d56f77d0b8284f97a6cc506a563c2920fb18df
Vidushi Krishna <vidushikris@gmail.com>
2025-09-06 14:41:49 +0800

    feat: 3d model

--- testapp3/ModelView1.swift
+++ testapp3/ModelView1.swift
@@ -19,13 +19,24 @@ struct ModelView1 : View {
             model.scale = [0.01, 0.01, 0.01]
             model.position = [0, 0.05, -0.5]
             
+            let model1 = try! await ModelEntity(named: "low_poly_person")
+            model1.scale = [0.001, 0.001, 0.001]
+            model1.position = [0, 0.01, 0.5]
+            
             // Anchor on a horizontal plane
             let anchor = AnchorEntity(.plane(.horizontal,
                                              classification: .any,
                                              minimumBounds: [0.2, 0.2]))
+            let anchor1 = AnchorEntity(.plane(.horizontal,
+                                             classification: .any,
+                                             minimumBounds: [0.1, 0.1]))
             anchor.addChild(model)
+            anchor.addChild(model1)
             content.add(anchor)
+            content.add(anchor1)
+            
             self.model = model
+            self.model = model1
             
             // Add camera anchor (follows the headset automatically)
             let cameraAnchor = AnchorEntity(.camera)
