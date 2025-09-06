2150f2c4415a65d0e14fd04fb528ff87975fd6c9
shivcabni <shibani>
2025-09-06 15:05:15 +0800

    feat: merge shivani chagnes

---
+++ testapp3/figure that screams 2.swift
@@ -0,0 +1,18 @@
+////
+////  figure that screams.swift
+////  testapp3
+////
+////  Created by Shivanishri on 6/9/25.
+////
+//
+//import SwiftUI
+//
+//struct figure_that_screams: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//#Preview {
+//    figure_that_screams()
+//}
